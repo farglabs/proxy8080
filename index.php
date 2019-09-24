4e4615e94dbfaca706129485949fbbe82114be74
farglabs <martel@vilmal.com>
2019-09-24 08:35:36 -0700

    Added config.ini with ability to set port and web directory path Added basic error checking

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
 // index.php - main file for proxy8080
 // last modified by farglabs on 2019-09-23
-if( $_SERVER[REQUEST_URI] == '/proxy8080/' ) {
-	echo `curl http://127.0.0.1:8080`;
-} else if( $_SERVER[REQUEST_URI] == '/proxy8080/index.php' ) {
-	echo `curl http://127.0.0.1:8080`;
-} else if( substr( $_SERVER[REQUEST_URI], 0, strlen('/proxy8080/index.php') ) == '/proxy8080/index.php' ) {
-	$path = $_SERVER[REQUEST_URI];
-	$path = substr( $path, strlen('/proxy8080/index.php') );
-	echo `curl http://127.0.0.1:8080$path`;
-} else if( substr( $_SERVER[REQUEST_URI], 0, strlen('/proxy8080') ) == '/proxy8080' ) {
-	$path = $_SERVER[REQUEST_URI];
-	$path = substr( $path, strlen('/proxy8080') );
-	echo `curl http://127.0.0.1:8080$path`;
+
+$iniPath = './config.ini'; // Default location of config.ini is in same directory as index.php
+$iniArray = '';
+$proxiedPort = "8080"; // These defaults should be overwritten by your config.ini file!
+$proxyPath = "proxy8080"; // These defaults should be overwritten by your config.ini file!
+
+try {
+	$iniArray = parse_ini_file( $iniPath );
+	$proxiedPort = $iniArray["proxiedPort"];
+	$proxyPath = $iniArray["proxyPath"];
+} catch( Exception $iniError ) {
+	echo 'Error parsing your config.ini file: ', $iniError->getMessage();
+}
+
+if( gettype($proxiedPort) == "string" && gettype($proxyPath) == "string" ) {
+	try {
+		proxyRequest($proxiedPort, $proxyPath);
+	} catch( Exception $proxyError ) {
+		echo 'Error proxying your site: ', $proxyError->getMessage();
+	}
 } else {
-	echo "Invalid request or something...";
+	echo 'Error: Check your config.ini settings.';
+}
+
+function proxyRequest($port, $webPath) {
+	if( $_SERVER[REQUEST_URI] == '/' . $webPath . '/' ) {
+		echo `curl http://127.0.0.1:$port`;
+	} else if( $_SERVER[REQUEST_URI] == '/' . $webPath . '/index.php' ) {
+		echo `curl http://127.0.0.1:$port`;
+	} else if( substr( $_SERVER[REQUEST_URI], 0, strlen('/' . $webPath . '/index.php') ) == '/' . $webPath . '/index.php' ) {
+		$path = $_SERVER[REQUEST_URI];
+		$path = substr( $path, strlen('/' . $webPath . '/index.php') );
+		echo `curl http://127.0.0.1:$port$path`;
+	} else if( substr( $_SERVER[REQUEST_URI], 0, strlen('/' . $webPath ) ) == '/' . $webPath ) {
+		$path = $_SERVER[REQUEST_URI];
+		$path = substr( $path, strlen('/' . $webPath) );
+		echo `curl http://127.0.0.1:$port$path`;
+	} else {
+		echo 'Invalid request or something...';
+	}
 }
 ?>
